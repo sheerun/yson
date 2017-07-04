a1c85bf6d029ceca996afb0f6113faf135c8476a
Adam Stankiewicz <sheerun@sher.pl>
2017-07-04 03:29:13 +0200

    Simplify EachKey and EachValue

--- yson.go
+++ yson.go
@@ -4,26 +4,6 @@ import (
 	"github.com/buger/jsonparser"
 )
 
-// Iterates though each key in json. Does nothing if json is not an object.
-//
-// Memory usage: O(1), Time usage: O(n)
-func EachKey(json []byte, fn func(key []byte)) {
-	jsonparser.ObjectEach(json, func(key []byte, value []byte, t jsonparser.ValueType, offset int) error {
-		fn(key)
-		return nil
-	})
-}
-
-// Iterates though each value in json. Does nothing if json is not an object.
-//
-// Memory usage: O(1), Time usage: O(n)
-func EachValue(json []byte, fn func(value []byte)) {
-	jsonparser.ObjectEach(json, func(key []byte, value []byte, t jsonparser.ValueType, offset int) error {
-		fn(value)
-		return nil
-	})
-}
-
 // Iterates though each key and value in json. Does nothing if json is not an object.
 //
 // Use it for iterating over huge input, as it uses GC better than yson.EachStringKeyAndValue(json)
@@ -33,5 +13,23 @@ func EachPair(json []byte, fn func(key []byte, value []byte)) {
 	jsonparser.ObjectEach(json, func(key []byte, value []byte, t jsonparser.ValueType, offset int) error {
 		fn(key, value)
 		return nil
+	})
+}
+
+// Iterates though each key in json. Does nothing if json is not an object.
+//
+// Memory usage: O(1), Time usage: O(n)
+func EachKey(json []byte, fn func(key []byte)) {
+	EachPair(json, func(key []byte, value []byte) {
+		fn(key)
+	})
+}
+
+// Iterates though each value in json. Does nothing if json is not an object.
+//
+// Memory usage: O(1), Time usage: O(n)
+func EachValue(json []byte, fn func(value []byte)) {
+	EachPair(json, func(key []byte, value []byte) {
+		fn(value)
 	})
 }
